a5f6f0117492f0fb9d0dbbfd67b3a6de0a63610f
Denis <denlapt@gmail.com>
2020-05-23 01:37:03 +0300

    fix scroll bugs

--- app/js/main.js
+++ app/js/main.js
@@ -15,20 +15,29 @@ function updateSectionMap() {
   window.sectionMap = { $prev, $current, $next };
 }
 
-function makeScroll(newTop, speed = 15, dir, count) {
+function makeScroll($section, step = 20, dir, count) {
+  if(!$section) {
+    window.wheelBlocked = false;
+    return false;
+  }
+
+  const newTop = $section.offsetTop;
+
   if (count === undefined) {
     count = winTop;
     dir = winTop <= newTop ? 1 : -1;
   }
   // If current point is end position => stop recursion
-  if (winTop >= newTop && dir === 1 || winTop <= newTop && dir === -1) {
+  if (winTop + step >= newTop && dir === 1 || winTop - step <= newTop && dir === -1) {
+    window.scrollTo(0, newTop);
+    window.wheelBlocked = false;
     return false;
   };
 
   requestAnimationFrame(() => {
     window.scrollTo(0, count);
-    count = winTop < newTop ? count + speed : count - speed;
-    makeScroll(newTop, speed, dir, count);
+    count = winTop < newTop ? count + step : count - step;
+    makeScroll($section, step, dir, count);
   });
 }
 
@@ -37,28 +46,23 @@ function addShadow($section, shade) {
 
   $section.style.zIndex = '2';
   sectionMap.$current.style.filter = `blur(${Math.floor(shade / 100)}px)`;
-  $section.style.boxShadow = `0 0 500px ${shade}px rgba(0, 0, 0)`;
+  $section.style.boxShadow = `0 0 ${shade * 0.75}px ${shade / 2}px rgba(255, 255, 255)`;
 }
 
-function onScrollEnd(cb, timeout) {
-  window.timerEnd;
-  document.addEventListener('wheel', () => {
-    if (window.timerEnd) {
-      clearTimeout(window.timerEnd);
-    }
-    window.timerEnd = setTimeout(cb, timeout);
-  })
+function onWheel(ev) {
+  window.winTop = document.documentElement.scrollTop;
+
+  if (window.wheelBlocked) return false;
+  window.scrollDir = ev.deltaY;
+  window.wheelBlocked = true;
+
+  updateSectionMap();
+  makeScroll(window.scrollDir > 0 ? sectionMap.$next : sectionMap.$prev);
 }
 
-onScrollEnd(() => {
-
-}, 100);
-
-window.winTop = document.documentElement.scrollTop;
+document.addEventListener('wheel', onWheel);
 document.addEventListener('scroll', () => {
   window.winTop = document.documentElement.scrollTop;
-  window.scrollDir = window.scrollY <= window.prevScroll ? -1 : 1;
-  window.prevScroll = window.scrollY;
   updateSectionMap();
   if (sectionMap.$current) {
     sectionMap.$current.style.zIndex = '2';
